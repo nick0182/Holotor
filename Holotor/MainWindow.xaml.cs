af2d3966ad07e00f1b5c46acc45afb039db3f55e
n.shaidulin <n.shaidulin@textback.io>
2023-08-17 08:10:16 +0300

    Media is playing

--- Holotor/MainWindow.xaml.cs
+++ Holotor/MainWindow.xaml.cs
@@ -4,6 +4,8 @@ using Windows.UI.ViewManagement;
 using Holotor.Core.Services;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using Microsoft.UI.Xaml.Controls;
+using Windows.Media.Core;
 
 namespace Holotor;
 
@@ -12,15 +14,15 @@ public sealed partial class MainWindow : WindowEx
     public MainWindow()
     {
         InitializeComponent();
-        var temperatureService = new TemperatureService();
-        // var cts = new CancellationTokenSource();
-        Task.Run(async () =>
-        {
-            while (true)
-            {
-                Debug.WriteLine("{0}; Current CPU temperature: {1}", Environment.CurrentManagedThreadId, temperatureService.GetTemperature());
-                await Task.Delay(TimeSpan.FromSeconds(1));
-            }
-        });
+        //var temperatureService = new TemperatureService();
+        ////var cts = new CancellationTokenSource();
+        //Task.Run(async () =>
+        //{
+        //    while (true)
+        //    {
+        //        Debug.WriteLine("{0}; Current CPU temperature: {1}", Environment.CurrentManagedThreadId, temperatureService.GetTemperature());
+        //        await Task.Delay(TimeSpan.FromSeconds(1));
+        //    }
+        //});
     }
 }
